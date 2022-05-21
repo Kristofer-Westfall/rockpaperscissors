30298a66a49b2435c7d83ee73b8570ef23fe1aaa
Kristofer-Westfall <Kristoferwestfall@gmail.com>
2022-05-20 20:51:01 -0400

    This works.  Just have to implement to something usable by the user.

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
+let compChoice = 0;
+
 const choiceButton = document.querySelectorAll('[data-choice');
 choiceButton.forEach(choiceButton => {
     choiceButton.addEventListener('click', () =>{
      const choiceName =  choiceButton.dataset.choice;
-     makeChoice(choiceName);
-
+     computerChoice();
+     isWinner(choiceName, compChoice);
     })
 })
-let compChoice = 0;
-
 
 function computerChoice(){
     let i = Math.floor(Math.random() * 3);
     return compChoice = options[i];
-}
-
-function choiceAssign(){
-    let choice = function(){
-        if (choiceName == options.context)  {
-            return options.context;
-        }
-    }
-}
-
-function isWinner(choice, compPlay){
-    if (choice.beats === compPlay.context){
-        console.log("You win!");
-    } else{
-        console.log("you lose!");
-    }
 }
 
 const options=[{
@@ -44,9 +28,12 @@ const options=[{
 }
 ]
 
-function makeChoice(){
-    choiceAssign();
-    const compPlay = computerChoice();
-    const youWin = isWinner(choice, compPlay);
-    const computerWin = isWinner(compPlay, choice);
+function isWinner(choiceName, compChoice) {
+    if (choiceName == compChoice.beats){
+        console.log("you lose!");
+    } else if (choiceName == compChoice.context){
+        console.log("tie!")
+    } else {
+        console.log("You win!")
+    }
 }
